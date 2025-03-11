e188dff6efeece6ce466054ce6938b28f449ca18
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2025-03-11 14:16:30 +0000

    Task 2 was added

--- task_2/main.py
+++ task_2/main.py
@@ -1,2 +1,16 @@
-def generator_numbers():
-    pass
+import re
+from typing import Callable, Generator
+
+def generator_numbers(text: str) -> Generator:
+    number_strings = re.findall(r'[0-9]+\.[0-9]+', text)
+
+    for number_string in number_strings:
+        yield float(number_string)
+
+def sum_profit(text: str, func: Callable) -> float:
+    sum = 0
+
+    for number in func(text):
+        sum += number
+
+    return sum
